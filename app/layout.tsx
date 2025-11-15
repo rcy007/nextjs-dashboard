3ae8b8e6a130178e445c042279c7a3ba0104b492
Abhinav Ahuja <ahujaaa@amazon.com>
2025-11-16 04:10:31 +0530

    lala

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import SideNav from './ui/dashboard/sidenav';
 
 
 export default function RootLayout({
@@ -9,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
