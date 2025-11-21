4737fc3f0f6474fcfbedc51d718c3246feed7152
Abhinav Ahuja <ahujaaa@amazon.com>
2025-11-22 05:27:43 +0530

    Remaining form validation. Pending - DO NOT RESET FORM on failed validation!!

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,6 @@ import {z} from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
-import { error } from 'console';
 
 export type State = {
     errors?: {
@@ -67,12 +66,21 @@ export async function createInvoice(prevState: State, formData: FormData){
 
 }
 
-export async function updateInvoice(id: string, formData: FormData){
-    const {customerId, amount, status} = UpdateInvoice.parse({
+export async function updateInvoice( id: string, prevState: State, formData: FormData){
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if(!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.'
+        };
+    }
+    
+    const {customerId, amount, status} = validatedFields.data;
 
     const amountInCents = amount * 100;
     try {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions'; 
+import { updateInvoice, State } from '@/app/lib/actions'; 
+import { useActionState } from 'react';
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,8 +21,10 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const initialState: State = {message: null, errors: {}};
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -44,6 +48,14 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <div id='customer-error' aria-live='polite' aria-atomic='true'>
+              {state.errors?.customerId && 
+              state.errors.customerId.map((error: string) => (
+                <p className='mt-2 text-sm text-red-500' key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
           </div>
         </div>
 
@@ -65,6 +77,14 @@ export default function EditInvoiceForm({
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id='ammount-error' aria-live='polite' aria-atomic='true'>
+              {state.errors?.amount && 
+              state.errors.amount.map((error: string) => (
+                <p className='mt-2 text-sm text-red-500' key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -110,7 +130,23 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div id='status-error' aria-live='polite' aria-atomic='true'>
+              {state.errors?.status && 
+              state.errors.status.map((error: string) => (
+                <p className='mt-2 text-sm text-red-500' key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
       </div>
+      <div id='overall-error' aria-live='polite' aria-atomic='true'>
+              {
+                state.message &&
+                  <p className='mt-2 text-sm text-red-500'>
+                  {state.message}
+                </p> 
+              }
+        </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
